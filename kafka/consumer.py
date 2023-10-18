edd8bc656991256925055c62f01e16dea1bb7e3f
maixdu <mmacieksitko@gmail.com>
2023-10-18 18:56:09 +0200

    feat: add addresses query to stream consumer and reorganize storing checkpoints

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
 import pyspark.sql.types as T
+import pyspark.sql.functions as F
 import findspark
 
 BlockSchema = T.StructType([
@@ -15,6 +16,11 @@ TxSchema = T.StructType([
     T.StructField("to", T.StringType(), True),
     T.StructField("value", T.StringType(), True),
     T.StructField("blockNumber", T.StringType(), True),
+])
+
+AddressSchema = T.StructType([
+    T.StructField("address", T.StringType(), True),
+    T.StructField("type", T.StringType(), True),
 ])
 
 findspark.init()
@@ -61,9 +67,9 @@ blocks_query = (blocks
          .format("org.neo4j.spark.DataSource")
          .option("labels", ":Block")
          .option("node.keys", "number")
-         .option("checkpointLocation", "./kafka/checkpoint") 
+         .option("checkpointLocation", "./kafka/checkpoints/1") 
          .outputMode("append")
-         .trigger(continuous="10 seconds")
+         .trigger(processingTime="5 seconds")
          .start()
 )
 
@@ -74,17 +80,57 @@ blocks_query = (blocks
 # query.stop()
 
 tx_query = (txs
-         .writeStream
-         .format("org.neo4j.spark.DataSource")
-         .option("labels", ":Tx")
-         .option("node.keys", "hash")
-         .option("checkpointLocation", "./kafka/checkpoint") 
-         .outputMode("append")
-         .trigger(continuous="5 seconds")
-         .start()
+  .writeStream
+  .format("org.neo4j.spark.DataSource")
+  .option("labels", ":Tx")
+  .option("node.keys", "hash")
+  .option("checkpointLocation", "./kafka/checkpoints/2") 
+  .outputMode("append")
+  .trigger(processingTime="5 seconds")
+  .start()
 )
 
+tx_block_relation_query = (txs.writeStream
+  .format("org.neo4j.spark.DataSource")
+  .option("query", """MATCH (b:Block)
+    MATCH (t:Tx {blockNumber: toString(b.number)})
+    MERGE (b)-[:HAS_COINBASE {value: t.value}]->(t)-[:INCLUDED_IN]->(b)""")
+  .option("checkpointLocation", "./kafka/checkpoints/3") 
+  .outputMode("append")
+  .trigger(processingTime="5 seconds")
+  .start())
+
+addresses = (txs
+              .select(F.explode(F.array("from", "to"))
+                      .alias("address"))
+              .filter(F.col('address').isNotNull())
+              .distinct()
+              .withColumn('type',
+                          F.when(col('address') == '0x', 'contract')
+                          .otherwise('private')))
+
+addresses_query = (addresses
+  .writeStream
+  .format("org.neo4j.spark.DataSource")
+  .option("labels", ":Address")
+  .option("node.keys", "address")
+  .option("checkpointLocation", "./kafka/checkpoints/4") 
+  .outputMode("append")
+  .trigger(processingTime="5 seconds")
+  .start()
+)
+
+tx_address_relation_query = (addresses.writeStream
+  .format("org.neo4j.spark.DataSource")
+  .option("query",
+    """MATCH (t:Tx), (a_from:Address {address: t.from}), (a_to:Address {address: t.to})
+    MERGE (a_from)-[:SENT {value: t.value}]->(t)-[:RECEIVED {value: t.value}]->(a_to)""")
+  .option("checkpointLocation", "./kafka/checkpoints/5") 
+  .outputMode("append")
+  .trigger(processingTime="5 seconds")
+  .start())
 
 print("Running consumer...")
-tx_query.awaitTermination()
+
+spark.streams.awaitAnyTermination()
 
